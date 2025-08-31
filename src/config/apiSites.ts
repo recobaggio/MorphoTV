35bea007a39a0a64999421c7c8bf5c6fdce71447
recobaggio <36385833+recobaggio@users.noreply.github.com>
2025-08-31 14:18:29 +0800

    Update apiSites.ts

--- src/config/apiSites.ts
+++ src/config/apiSites.ts
@@ -1,10 +1,5 @@
 export const API_SITES = [
   {
-    key: "heimuer",
-    api: "https://json.heimuer.xyz",
-    name: "黑木耳",
-  },
-  {
     key: "wolong",
     api: "https://wolongzyw.com",
     name: "卧龙资源",
@@ -13,10 +8,5 @@ export const API_SITES = [
     key: "jisu",
     api: "https://jszyapi.com",
     name: "极速资源"
-  },
-  {
-    key: "ffzy5",
-    api: "http://ffzy5.tv",
-    name: "非凡影视"
   },
 ];
